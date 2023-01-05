7a13b9b4e9193b5fd511f976e8a78618efbee869
m-bednarczyk <m.bednarczyk@samsung.com>
2023-01-05 21:19:09 +0100

    filter rooms

--- src/main/java/com/roomreservation/reservationservice/rest/RoomController.java
+++ src/main/java/com/roomreservation/reservationservice/rest/RoomController.java
@@ -4,9 +4,11 @@ import com.roomreservation.reservationservice.core.service.RoomService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
 import java.util.List;
 
 @RequestMapping("/rooms")
@@ -31,8 +33,14 @@ public class RoomController {
      * @return
      */
     @GetMapping("/all")
-    public List<Object> getAllRoomTypes() {
-        return roomService.getAllRooms();
+    public List<Object> getAllRoomTypes(
+            @RequestBody(required = false) Integer guestsNum,
+            @RequestBody(required = false) Integer daysNum,
+            @RequestBody(required = false) Date startDate) {
+        if (guestsNum == null || guestsNum == 0) {
+            guestsNum = 1;
+        }
+        return roomService.getAllRooms(guestsNum, daysNum, startDate);
     }
 
     @GetMapping("/{roomType}/details")
