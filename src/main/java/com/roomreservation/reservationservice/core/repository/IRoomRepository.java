8661f52e9a4c4b96ac48ef15a38767f25b59feb6
m-bednarczyk <m.bednarczyk@samsung.com>
2023-01-10 12:32:09 +0100

    Implement /rooms/all endpoint

--- src/main/java/com/roomreservation/reservationservice/core/repository/IRoomRepository.java
+++ src/main/java/com/roomreservation/reservationservice/core/repository/IRoomRepository.java
@@ -3,8 +3,7 @@ package com.roomreservation.reservationservice.core.repository;
 import com.roomreservation.reservationservice.core.domain.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.math.BigInteger;
 
-public interface IRoomRepository extends JpaRepository<RoomEntity, BigInteger> {
+public interface IRoomRepository extends JpaRepository<RoomEntity, Long> {
 
 }
