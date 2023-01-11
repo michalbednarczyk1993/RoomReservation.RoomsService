7861751fa44dab0eca69a5c75505b8fc08612e2b
m-bednarczyk <m.bednarczyk@samsung.com>
2023-01-11 14:15:41 +0100

    Update javadoc in controller

--- src/main/java/com/roomreservation/reservationservice/rest/RoomController.java
+++ src/main/java/com/roomreservation/reservationservice/rest/RoomController.java
@@ -19,7 +19,7 @@ public class RoomController {
     }
 
     /**
-     * Return all room types available in hotel with selected criteria.
+     * Return all room types available in hotel with selected criteria in JSON format.
      * In case when guestNumber is equal to null, then default value (1) is provided.
      * In case when daysNumber or startDate is equal to null, then system will return all possible values
      * that suit to guestNumber condition.
@@ -52,25 +52,49 @@ public class RoomController {
     }
 
     /**
-     * Wyświetl szczegółowe informacje o typie pokoju
+     * Return detail information about selected room type in JSON format.
+     * In case of currency will be null, then default value (PLN) is provided.
+     * In case of selectedServices will be null none of them will be counted in.
+     * In case of selectedDate will be null, then first possible day will be chosen.
+     * In case of numberOfDays will be null, then default value (1) will be provided.
+     * In case of selectedMonth will be null, then month of first possible date will be chosen.
+     * INFO: Service could return selected_date that differ from provided selectedDate
+     * if those was not available for this room type.
+     * JSON structure:
+     * {
+     *      room_type.description: room type description
+     *      room_type.photos: list of hrefs to data storage with room type photo
+     *      room_type.selected_services: list of selected extra services
+     *      room_type.counted_price: total price of reservation
+     *      room_type.selected_date: selected date of stay (YYYY-MM-DD)
+     *      room_type.number_of_days: number of days chosen for stay
+     *      room_type.free_dates: list of comma separated free dates (in selected_month and next) of selected rooms
+     * }
      *
-     * Przypadek umożliwia [użytkownikowi] przejrzenie większej ilości informacji na temat [typu pokoju].
-     * W efekcie [użytkownik] jest przekierowany do innego widoku gdzie ma dostęp do większej ilości informacji na
-     * temat wybranego [typu pokoju].
-     * System prezentuje następujące informację: przejrzeć poglądowe zdjęcia dla wybranego [typu pokoju],
-     * przeczytać opis [typu pokoju], przejrzeć i wybrać możliwe [formy pobytu], zobaczyć obliczoną [cenę pobytu],
-     * przejrzeć dostępne terminy. [(Obliczenie ceny pobytu)] jest wykonywane automatycznie przy zmianie zaznaczonych
-     * [form pobytu].
+     * @param roomTypeId - id of selected room type
+     * @param currency - currency in which user want to pay for reservation. Possible values: PLN, EUR, USD, CHF
+     * @param selectedServices - comma separated extra paid services:
+     *                      bb - breakfast included into price
+     *                      di - dinner included into price
+     *                      allIn - everything included into price
+     *                      clean - everyday cleaning service included into price
+     *                      sport - sport equipment rental included into price
+     * @param selectedDate - selected date of stay (YYYY-MM-DD)
+     * @param numberOfDays - number of days chosen for stay
+     * @param selectedMonth - selected month to return "free_dates"
+     * @return JSON object that represent detailed information about selected room type
      */
     @GetMapping("/{roomTypeId}/details")
     public String getRoomTypeDetails(
             @PathVariable Long roomTypeId,
-            @RequestBody String currency,
-            @RequestBody String chosenOption) {
+            @RequestBody(required = false) String currency,
+            @RequestBody(required = false) String selectedServices,
+            @RequestBody(required = false) Date selectedDate,
+            @RequestBody(required = false) Integer numberOfDays,
+            @RequestBody(required = false) Integer selectedMonth) {
 
         String result = roomService.getRoomTypeDetails(roomTypeId);
 
         return null;
     }
-
 }
