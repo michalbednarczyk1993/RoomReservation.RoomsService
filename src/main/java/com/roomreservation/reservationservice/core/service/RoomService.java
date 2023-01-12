5f1d6f1b043a633cfe1ed03495d1182914d529b9
m-bednarczyk <m.bednarczyk@samsung.com>
2023-01-12 12:55:26 +0100

    Update code related to "/{roomTypeId}/details"

--- src/main/java/com/roomreservation/reservationservice/core/service/RoomService.java
+++ src/main/java/com/roomreservation/reservationservice/core/service/RoomService.java
@@ -1,28 +1,22 @@
 package com.roomreservation.reservationservice.core.service;
 
 import com.roomreservation.reservationservice.core.domain.RoomTypeEntity;
-import com.roomreservation.reservationservice.core.repository.IPhotoLinkRepository;
-import com.roomreservation.reservationservice.core.repository.IRoomRepository;
 import com.roomreservation.reservationservice.core.repository.IRoomTypeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Slf4j
 @Service
 public class RoomService {
-    private final IRoomRepository roomRepo;
     private final IRoomTypeRepository roomTypeRepo;
-    private final IPhotoLinkRepository photoLinkRepo;
 
-    RoomService(IRoomRepository roomRepository, IRoomTypeRepository roomTypeRepository, IPhotoLinkRepository photoLinkRepository) {
-        this.roomRepo = roomRepository;
+    RoomService(IRoomTypeRepository roomTypeRepository) {
         this.roomTypeRepo = roomTypeRepository;
-        this.photoLinkRepo = photoLinkRepository;
     }
 
     public List<Object> getAllRooms(Integer guestsNum, Integer daysNum, Date startDate) {
@@ -48,17 +42,39 @@ public class RoomService {
         return result.toList();
     }
 
-    public String getRoomTypeDetails(Long roomTypeId) {
-        RoomTypeEntity roomType = roomTypeRepo.getReferenceById(roomTypeId);
+    public String getRoomTypeDetails(Long roomTypeId, String currency, String selectedServices, Date selectedDate,
+                                     Integer numberOfDays, Integer numberOfGuests, Integer selectedMonth)
+    {
         JSONObject result = new JSONObject();
-
-        result.put("name", roomType.getName());
-        result.put("description", roomType.getDescription());
-        result.put("photos", roomType.getPhotoLinks());
-        // TODO: add "additional service" options
-        // TODO: count price at another service
+        getRoomTypeData(roomTypeId).forEach(pair -> result.put(pair.getFirst().toString(), pair.getSecond()));
+        result.put("room_type.counted_price", getCountedPrice(roomTypeId, currency, selectedServices, numberOfDays, numberOfGuests));
+        getReservationData(selectedDate, numberOfDays, selectedMonth).forEach(pair -> result.put((String) pair.getFirst(), pair.getSecond()));
 
         return result.toString();
+    }
+
+    private List<Pair<?,?>> getRoomTypeData(Long roomTypeId) {
+        RoomTypeEntity roomType = roomTypeRepo.getReferenceById(roomTypeId);
+        List<Pair<?,?>> result = new ArrayList<>();
+        result.add(Pair.of("room_type.name", roomType.getName()));
+        result.add(Pair.of("room_type.name", roomType.getName()));
+        result.add(Pair.of("room_type.capacity", roomType.getCapacity()));
+        result.add(Pair.of("room_type.description", roomType.getDescription()));
+        result.add(Pair.of("room_type.photos", roomType.getPhotoLinks()));
+        return result;
+    }
+
+    private List<Pair<?,?>> getReservationData(Date selectedDate, Integer numberOfDays, Integer selectedMonth) {
+        List<Pair<?,?>> result = new ArrayList<>();
+        //TODO refer to Rest API of ReservationService
+        result.add(Pair.of("reservation_data", "connection not ready yet"));
+        return result;
+    }
+
+    private Double getCountedPrice(Long roomTypeId, String currency, String selectedServices,
+                                      Integer numberOfDays, Integer numberOfGuests) {
+        //TODO refer to Rest API of PaymentService
+        return roomTypeRepo.getReferenceById(roomTypeId).getBasePrice();
     }
 
     private List<RoomTypeEntity> filterRoomTypes(Integer guestsNum, Integer daysNum, Date startDate) {
@@ -72,7 +88,6 @@ public class RoomService {
           TODO: add connection to RoomReservation.OperationsAtReservations service.
            There should be also at least one room of selected type since startDate for selected number of days (daysNum).
          */
-
         log.info("There are " + roomTypes.size() + " rooms that match selected filters");
 
         return roomTypes;
@@ -85,7 +100,6 @@ public class RoomService {
 
         List<RoomTypeEntity> roomTypes = roomTypeRepo.findAll().stream()
                 .filter(roomType -> roomType.getCapacity() >= guestsNum).toList();
-
         log.info("There are " + roomTypes.size() + " rooms that match selected filters");
 
         return roomTypes;
