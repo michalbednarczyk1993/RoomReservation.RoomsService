236521c8affa92857483aefc7c5e8c2e13d8eb68
michalbednarczyk1993 <michalbednarczyk1993@gmail.com>
2023-10-23 13:25:18 +0200

    Inital conversion to Quarkus project

--- src/main/java/com/roomreservation/roomservice/RoomServiceApplication.java
+++ src/main/java/com/roomreservation/roomservice/RoomServiceApplication.java
@@ -1,47 +1,50 @@
 package com.roomreservation.roomservice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
-import java.util.Collections;
-
-@EnableSwagger2
-@SpringBootApplication
+@Path("/hello")
 public class RoomServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoomServiceApplication.class, args);
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "Hello from RESTEasy Reactive";
 	}
-
-	@Bean
-	public Docket get() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build()
-				.apiInfo(createApiInfo());
-	}
-
-	private ApiInfo createApiInfo() {
-		return new ApiInfo(
-				"RoomReservation.RoomService API",
-				"opis...",
-				"0.0.1",
-				"",
-				new Contact("Michal Bednarczyk", "", "michal.bednarczyk1993@gmail.com"),
-				"Apache License 2.0",
-				"https://github.com/michalbednarczyk1993/RoomReservation.RoomsService/blob/master/LICENSE",
-				Collections.emptyList()
-		);
-	}
-
 }
+
+
+//@EnableSwagger2
+//@SpringBootApplication
+//public class RoomServiceApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(RoomServiceApplication.class, args);
+//	}
+//
+//	@Bean
+//	public Docket get() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.select()
+//				.apis(RequestHandlerSelectors.any())
+//				.paths(PathSelectors.any())
+//				.build()
+//				.apiInfo(createApiInfo());
+//	}
+//
+//	private ApiInfo createApiInfo() {
+//		return new ApiInfo(
+//				"RoomReservation.RoomService API",
+//				"opis...",
+//				"0.0.1",
+//				"",
+//				new Contact("Michal Bednarczyk", "", "michal.bednarczyk1993@gmail.com"),
+//				"Apache License 2.0",
+//				"https://github.com/michalbednarczyk1993/RoomReservation.RoomsService/blob/master/LICENSE",
+//				Collections.emptyList()
+//		);
+//	}
+//
+//}
